c90997051c970886a5a8ee25224672a96f8f6238
Carsten Blüm <carsten@bluem.net>
2014-11-03 23:28:22 +0100

    New icon in demo, move functions to pashua.sh

--- example.sh
+++ example.sh
@@ -1,113 +1,38 @@
 #!/bin/bash
 
-#
-# USAGE INFORMATION:
-# As you can see this text, you obviously have opened the file in a text editor.
-# 
-# If you would like to *run* this example rather than *read* it, you
-# should open Terminal.app, drag this document's icon onto the terminal
-# window, bring Terminal.app to the foreground (if necessary) and hit return.
-# 
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Wrapper function for interfacing to Pashua. Written by Carsten
-# Bluem <carsten@bluem.net> in 10/2003, modified in 12/2003 (including
-# a code snippet contributed by Tor Sigurdsson), 08/2004 and 12/2004.
-# As 1st argument, you must pass a configuration string
-# As 2nd argument, you may pass the encoding to use (see documentation)
-# As 3rd argument, you may pass the path to the Pashua application,
-#                  if it's not in one of the standard locations.
-pashua_run() {
-
-	# Write config file
-	local pashua_configfile=`/usr/bin/mktemp /tmp/pashua_XXXXXXXXX`
-	echo "$1" > $pashua_configfile
-
-	# Find Pashua binary. We do search both . and dirname "$0"
-	# , as in a doubleclickable application, cwd is /
-	# BTW, all these quotes below are necessary to handle paths
-	# containing spaces.
-	local bundlepath="Pashua.app/Contents/MacOS/Pashua"
-	local pashuapath=""
-	if [ "$3" = "" ]
-	then
-		mypath=`dirname "$0"`
-		for searchpath in "$mypath/Pashua" \
-		                  "$mypath/$bundlepath" \
-		                  "./$bundlepath" \
-						  "/Applications/$bundlepath" \
-						  "$HOME/Applications/$bundlepath"
-		do
-			if [ -f "$searchpath" -a -x "$searchpath" ]
-			then
-				pashuapath=$searchpath
-				break
-			fi
-		done
-	else
-		# Directory given as argument
-		pashuapath="$3/$bundlepath"
-	fi
-
-	if [ "" = "$pashuapath" ]
-	then
-		>&2 echo "Error: Pashua could not be found"
-		exit 1
-	fi
-
-	# Manage encoding
-	if [ "$2" = "" ]
-	then
-		local encoding=""
-	else
-		local encoding="-e $2"
-	fi
-
-	# Get result
-	local result=$("$pashuapath" $encoding $pashua_configfile | perl -pe 's/ /;;;/g;')
-
-	# Remove config file
-	rm $pashua_configfile
-
-	# Parse result
-	for line in $result
-	do
-		key=$(echo $line | sed 's/^\([^=]*\)=.*$/\1/')
-		value=$(echo $line | sed 's/^[^=]*=\(.*\)$/\1/' | sed 's/;;;/ /g')
-		varname=$key
-		varvalue="$value"
-		eval $varname='$varvalue'
-	done
-}
+# Include pashua.sh to be able to use the 2 functions defined in that file
+source "$MYDIR/pashua.sh"
 
 # Define what the dialog should be like
 # Take a look at Pashua's Readme file for more info on the syntax
 
 conf="
-# Set transparency: 0 is transparent, 1 is opaque
-*.transparency=0.95
-
 # Set window title
-*.title = Introducing Pashua
+*.title = Welcome to Pashua
 
 # Introductory text
-tb.type = text
-tb.default = Pashua is an application for generating dialog windows from programming languages which lack support for creating native GUIs on Mac OS X. Any information you enter in this example window will be returned to the calling script when you hit “OK”; if you decide to click “Cancel” or press “Esc” instead, no values will be returned.[return][return]This window demonstrates nine of the GUI widgets that are currently available. You can find a full list of all GUI elements and their corresponding attributes in the documentation that is included with Pashua.
-tb.height = 276
-tb.width = 310
-tb.x = 340
-tb.y = 44
+txt.type = text
+txt.default = Pashua is an application for generating dialog windows from programming languages which lack support for creating native GUIs on Mac OS X. Any information you enter in this example window will be returned to the calling script when you hit “OK”; if you decide to click “Cancel” or press “Esc” instead, no values will be returned.[return][return]This window shows nine of the UI element types that are available. You can find a full list of all GUI elements and their corresponding attributes in the documentation (➔ Help menu) that is included with Pashua.
+txt.height = 276
+txt.width = 310
+txt.x = 340
+txt.y = 44
+txt.tooltip = This is an element of type “text”
 
 # Add a text field
-tx.type = textfield
-tx.label = Example textfield
-tx.default = Textfield content
-tx.width = 310
+tf.type = textfield
+tf.label = Example textfield
+tf.default = Textfield content
+tf.width = 310
+tf.tooltip = This is an element of type “textfield”
 
 # Add a filesystem browser
 ob.type = openbrowser
 ob.label = Example filesystem browser (textfield + open panel)
 ob.width=310
-ob.tooltip = Blabla filesystem browser
+ob.tooltip = This is an element of type “openbrowser”
 
 # Define radiobuttons
 rb.type = radiobutton
@@ -115,8 +40,7 @@ rb.label = Example radiobuttons
 rb.option = Radiobutton item #1
 rb.option = Radiobutton item #2
 rb.option = Radiobutton item #3
-rb.option = Radiobutton item #4
-rb.default = Radiobutton item #2
+rb.tooltip = This is an element of type “radiobutton”
 
 # Add a popup menu
 pop.type = popup
@@ -126,54 +50,49 @@ pop.option = Popup menu item #1
 pop.option = Popup menu item #2
 pop.option = Popup menu item #3
 pop.default = Popup menu item #2
+pop.tooltip = This is an element of type “popup”
 
-# Add a checkbox
-chk1.type = checkbox
-chk1.label = Pashua offers checkboxes, too
-chk1.rely = -18
-chk1.default = 1
-
-# Add another one
+# Add 2 checkboxes
+chk.rely = -18
+chk.type = checkbox
+chk.label = Pashua offers checkboxes, too
+chk.tooltip = This is an element of type “checkbox”
+chk.default = 1
 chk2.type = checkbox
 chk2.label = But this one is disabled
 chk2.disabled = 1
+chk2.tooltip = Another element of type “checkbox”
 
 # Add a cancel button with default label
-cb.type=cancelbutton
+cb.type = cancelbutton
+cb.tooltip = This is an element of type “cancelbutton”
+
+db.type = defaultbutton
+db.tooltip = This is an element of type “defaultbutton” (which is automatically added to each window, if not included in the configuration)
 "
 
-# Set the images' paths relative to this file's path / 
-# skip images if they can not be found in this file's path
-icon=$(dirname "$0")'/.icon.png'
-bgimg=$(dirname "$0")'/.demo.png'
-
-if [ -e "$icon" ]
+# Get the icon from the application bundle
+locate_pashua
+bundlecontents=$(dirname $(dirname "$pashuapath"));
+if [ -e "$bundlecontents/Resources/AppIcon@2.png" ]
 then
-	# Display Pashua's icon
-	conf="$conf
-	     img.type = image
-	     img.x = 530
-	     img.y = 255
-	     img.path = $icon"
+    conf="$conf
+          img.type = image
+          img.x = 435
+          img.y = 248
+          img.maxwidth = 128
+          img.tooltip = This is an element of type “image”
+          img.path = $bundlecontents/Resources/AppIcon@2.png"
 fi
 
-if [ -e "$bgimg" ]
-then
- 	# Display background image
-	conf="$conf
-	      bg.type = image
-	      bg.x = 30
-	      bg.y = 2
-	      bg.path = $bgimg"
-fi
-
-pashua_run "$conf" 'utf8'
+pashua_run "$conf"
 
 echo "Pashua created the following variables:"
 echo "  tb  = $tb"
-echo "  tx  = $tx"
+echo "  tf  = $tf"
 echo "  ob  = $ob"
 echo "  pop = $pop"
 echo "  rb  = $rb"
 echo "  cb  = $cb"
+echo "  chk = $chk"
 echo ""
---
+++ pashua.sh
@@ -0,0 +1,70 @@
+
+# Tries to find the Pashua executable in one of a few default search locations or in
+# a custom path passed as optional argument. When it can be found, the filesystem
+# path will be in $pashuapath, otherwise $pashuapath will be empty. The return value
+# is 0 if it can be found, 1 otherwise.
+#
+# Argument 1: Path to a folder containing Pashua.app (optional)
+locate_pashua() {
+
+    local bundlepath="Pashua.app/Contents/MacOS/Pashua"
+    local mypath=`dirname "$0"`
+
+    pashuapath=""
+
+    if [ ! "$1" = "" ]
+    then
+        searchpaths[0]="$1/$bundlepath"
+    fi
+    searchpaths[1]="$mypath/Pashua"
+    searchpaths[2]="$mypath/$bundlepath"
+    searchpaths[3]="./$bundlepath"
+    searchpaths[4]="/Applications/$bundlepath"
+    searchpaths[5]="$HOME/Applications/$bundlepath"
+    
+    for searchpath in "${searchpaths[@]}"
+    do
+        if [ -f "$searchpath" -a -x "$searchpath" ]
+        then
+            pashuapath=$searchpath
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+# Function for communicating with Pashua
+#
+# Argument 1: Configuration string
+# Argument 2: Path to a folder containing Pashua.app (optional)
+pashua_run() {
+
+    # Write config file
+    local pashua_configfile=`/usr/bin/mktemp /tmp/pashua_XXXXXXXXX`
+    echo "$1" > $pashua_configfile
+
+    locate_pashua "$2"
+
+    if [ "" = "$pashuapath" ]
+    then
+        >&2 echo "Error: Pashua could not be found"
+        exit 1
+    fi
+
+    # Get result
+    local result=$("$pashuapath" $pashua_configfile | perl -pe 's/ /;;;/g;')
+
+    # Remove config file
+    rm $pashua_configfile
+
+    # Parse result
+    for line in $result
+    do
+        key=$(echo $line | sed 's/^\([^=]*\)=.*$/\1/')
+        value=$(echo $line | sed 's/^[^=]*=\(.*\)$/\1/' | sed 's/;;;/ /g')
+        varname=$key
+        varvalue="$value"
+        eval $varname='$varvalue'
+    done
+}
